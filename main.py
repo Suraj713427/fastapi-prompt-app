baf9754417b024b6c4b1c04d36abaafb140dd302
Suraj Paul <suryapaul713427@gmail.com>
2025-07-07 16:32:47 +0530

    Fix: updated main.py with working server

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from fastapi import FastAPI
 from routes import router
 
-app = FastAPI(
-    title="Prompt Backend API",
-    description="Backend test using FastAPI with login, prompt, and history",
-    version="1.0"
-)
-
+app = FastAPI()
 app.include_router(router)
